c3943d0aa4e39bfb3a49488fd1aa1874f8513ab9
Illia Polishchuk <iliya.polischuk22@gmail.com>
2025-10-12 13:17:32 -0400

    Add solution to 9917 Average Salaries

--- Python/pandas/09917_avarage_salaries.py
+++ Python/pandas/09917_avarage_salaries.py
@@ -1,1 +1,7 @@
 # 9917 Average Salaries 
+import pandas as pd
+
+employee.head()
+employee['average_salary'] = employee.groupby(['department'])['salary'].transform('mean')
+result = employee[['department', 'first_name', 'salary', 'average_salary']]
+result = result.sort_values(by='department')
