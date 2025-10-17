241ef802db6b9fb0c6f7f3a6dd80c733c70af0a6
Illia Polishchuk <iliya.polischuk22@gmail.com>
2025-10-17 13:05:19 -0400

    Add solution to 10087 Find all posts which were reacted to with a heart

---
+++ Python/pandas/10087_find_all_posts_which_were_reacted_to_with_a_heart.py
@@ -0,0 +1,8 @@
+# Find all posts which were reacted to with a heart
+import pandas as pd
+
+facebook_reactions.head()
+
+heart = facebook_reactions[facebook_reactions['reaction'] == 'heart'][['post_id']]
+result = pd.merge(heart, facebook_posts, on='post_id').drop_duplicates(subset='post_id')
+result
